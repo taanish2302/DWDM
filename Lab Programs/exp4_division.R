95184b73e58d4117132de3d3e3a259cbf9de0d7f
Taanish Sushil <75430328+taanish2302@users.noreply.github.com>
2026-08-13 14:25:03 +0530

    Add files via upload

---
+++ Lab Programs/exp4_division.R
@@ -0,0 +1,12 @@
+# EXPERIMENT-4
+# DIVISION
+#
+# AIM: To prove the program for division using R-tool.
+
+num1 <- as.integer(readline(prompt = "Enter the first number: "))
+num2 <- as.integer(readline(prompt = "Enter the second number: "))
+
+num3 <- num1 / num2
+print(num3)
+
+# RESULT: Thus the basic program for division is executed successfully.
